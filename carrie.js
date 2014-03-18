3ab9b8e0286157fb6bdfb919fe808fcd3dae0e0e
Justin Hough <JHough@esri.com>
2014-03-17 22:15:43 -0700

    Converting functions to use vanilla js instead of jQuery

--- carrie.js
+++ carrie.js
@@ -8,6 +8,33 @@
  *
  * ------------------------------------ */
 
+// HasClass
+function hasClass(elem, className) {
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// AddClass
+function addClass(elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += ' ' + className;
+  }
+}
+// RemoveClass
+function removeClass(elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  }
+}
+// ToggleState
+var toggleState = function (elem, one, two) {
+  var elem = document.querySelector(elem);
+  elem.setAttribute('data-state', elem.getAttribute('data-state') === one ? two : one);
+};
+
+
 // Declaring global vars for functions
 var option, navigation;
 
@@ -17,9 +44,9 @@ function carrieJS(option){
   var config = {
     CSS: {
       current: 'nav-active',
-      mobile: 'nav-mobile',
-      //.mobile-nav-btn
-      //.mobile-menu
+      mobileActive: 'nav-mobile',
+      mobileMenu: 'mobile-menu',
+      mobileBtn: 'mobile-nav-btn',
       //.sub-mobile-btn
       //.nav-parent
       //.sub-expand
@@ -28,7 +55,8 @@ function carrieJS(option){
       //.sub-nav-parent
     },
     IDs: {
-      container: 'navigation'
+      container: 'navigation',
+      
     },
     labels: {
       // labels here
@@ -40,10 +68,15 @@ function carrieJS(option){
   };
   
   o = config;
-  obj = $(option);
+  //obj = option;
   leng = 0;
   
-  container = '#'+o.IDs.container;
+  container = document.getElementById(o.IDs.container);
+  mobileContainer = document.getElementsByClassName(o.CSS.mobileMenu);
+  
+  // Remove after removing jQuery
+  jContainer = '#'+o.IDs.container;
+  
   
   function init(){
     //navBuilder();
@@ -72,14 +105,14 @@ function carrieJS(option){
   
   function currentNavItem(){
   
-    $(container).find('a').each(function(){
-      var path = $(this).attr('href');
-      
-      if(window.location.pathname.indexOf(path) != -1 && path.length > leng){
-        leng = path.length
-        $(this).addClass('nav-active').siblings().removeClass('nav-active');
+    var all_links = container.getElementsByTagName("a"),
+      i=0, len=all_links.length,
+      full_path = location.href.split('#')[0];
+    for(; i<len; i++) {
+      if(all_links[i].href.split("#")[0] == full_path) {
+        all_links[i].className += " nav-active";
       }
-    });
+    }
     
   };
   
@@ -87,47 +120,65 @@ function carrieJS(option){
     mobileBtn = '<span class="mobile-nav-btn">Menu</span>';
     subDropBtn = '<span class="sub-mobile-nav-icon"></span>';
     
-    $(container + ' ul:first-child').addClass('mobile-menu');
-    
-    $(container + ' ul li').addClass('sub-mobile-btn');
-    $(container + ' li > ul').parent().addClass('sub-expand').prepend(subDropBtn);
-    
-    $(container + ' ul ul').addClass('sub-mobile-menu');
-    $(container).prepend(mobileBtn);
+    // Convert
+    $(jContainer + ' ul:first-child').addClass('mobile-menu');
+    $(jContainer + ' ul li').addClass('sub-mobile-btn');
+    $(jContainer + ' li > ul').parent().addClass('sub-expand').prepend(subDropBtn);
+    $(jContainer + ' ul ul').addClass('sub-mobile-menu');
+    $(jContainer).prepend(mobileBtn);
   };
   
   function mobileNavMenu(){
-    $('.mobile-menu').hide();
-    $('.sub-mobile-menu').hide();
     
-    $('.mobile-nav-btn').click(function(){
-      $('.mobile-menu').toggle();
-    })
+    mobileOpenBtn = document.querySelector('.'+o.CSS.mobileBtn);
+    mobileOpenBtn.onclick = function (e) {
+      toggleState('.mobile-menu', 'closed', 'open');
+      e.preventDefault();
+    };
+
     
+    [].forEach.call(document.querySelectorAll('.sub-mobile-nav-icon'), function(el) {
+      el.addEventListener('click', function() {
+        
+        // Convert
+        // Hide all other menus
+        $('.sub-mobile-menu').hide();
+        $('.sub-mobile-btn').removeClass('sub-mobile-active');
+        // Show current menu
+        $(this).siblings('.sub-mobile-menu').toggle();
+        $(this).parent().toggleClass('sub-mobile-active');
+
+      })
+    });
+    
+/*
+    //jQuery Version
     $('.sub-mobile-nav-icon').click(function(){
+      // Hide all other menus
+      $('.sub-mobile-menu').hide();
+      $('.sub-mobile-btn').removeClass('sub-mobile-active');
+      // Show current menu
       $(this).siblings('.sub-mobile-menu').toggle();
       $(this).parent().toggleClass('sub-mobile-active');
     })
+*/
+
   };
 
   function screenAdapt(width) {
-    width = $(document).width();
+    width = window.innerWidth;
     if (width > 600) {
-      $(container).removeClass('nav-mobile');
+      removeClass(container, 'nav-mobile');
     }
     else {
-      $(container).addClass('nav-mobile');
+      addClass(container, 'nav-mobile');
     }
   }
   
   // Run Screensize Check
-  $(function() {
-      screenAdapt($(this).width());
-      $(window).resize(function() {
-        screenAdapt($(this).width());
-        //carrieJS('desktop');
-      });
-  });
+  window.onresize = function(){
+    screenAdapt(window.innerWidth);
+  }
 
   init();
   return{config:config, init:init}
@@ -135,3 +186,9 @@ function carrieJS(option){
   console.log('carrieJS has run');
   
 };
+
+
+// Start on Load
+document.addEventListener('DOMContentLoaded', function() {
+  carrieJS();
+})
